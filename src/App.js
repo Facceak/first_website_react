ed02c24e8de87f79ac02507c85c25a4c3afe3f31
FacceAk <facceak@gmail.com>
2022-02-19 20:45:04 +0100

    Start making the website responsive.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import SideBar from './components/sidebar/SideBar';
 import Home from './components/home_page/home';
+import Header from './components/header/header';
 
 export default function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Home picture={require('./components/home_page/HomePageAssets/It Takes Two.png')} button_value={["INSTALL GAME","ADD FAVORITE"]} info={["It Takes Two", information]}></Home>
       <SideBar user_first_name={"Facce"} user_last_name={"Ak"}></SideBar>
-      <div className="Feeds">
-        <Home picture={require('./components/home_page/HomePageAssets/It Takes Two.png')} button_value={["INSTALL NOW","ADD FAVORITE"]}></Home>
-      </div>
     </div>
   );
 }
+
+var information = "Begib dich in It Takes Two – einem genreübergreifenden Plattformer-Adventure mit purer Koop-Ausrichtung – auf die verrückteste Reise deines Lebens. Spielt zusammen als Cody und May, ein Paar mit Konfliktpotenzial, das durch einen Zauberspruch in Puppen verwandelt wurde."
--- src/components/home_page/home.jsx
+++ src/components/home_page/home.jsx
@@ -3,13 +3,19 @@ import React from "react";
 import "./home.css"
 
 
-export default function Home({picture, button_value}){
+export default function Home({picture, button_value, info}){
     return(
         <div className="feed_site">
             <div className="top_game">
-                <img className="top_game_picture" src={picture}/>
-                <button className="get_button">{button_value[0]}</button>
-                <button className="favorite_button">{button_value[1]}</button>
+                <img className="top_game_picture" src={picture} />
+                <div className="buttons_position">
+                    <button className="get_button">{button_value[0]}</button>
+                    <button className="favorite_button">{button_value[1]}</button>
+                </div>
+                <div className="top_game_information">
+                    <h1 className="top_game_title">{info[0]}</h1>
+                    <p>{info[1]}</p>
+                </div>
             </div>
         </div>
     );
