32af51940e153ea55ac3a4e9101cc05ffe4fdbfb
FacceAk <facceak@gmail.com>
2022-02-19 13:23:33 +0100

    Redesign the SideBar.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <SideBar></SideBar>
+      <a className='copyright' href="https://icons8.com/icon/123420/suche-in-der-cloud">All SVGs from Icons8</a>
     </div>
+    
   );
 }
 
